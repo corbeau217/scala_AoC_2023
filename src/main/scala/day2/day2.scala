46cd36a800216831de66143cabe9e7da47c704bf
Corbeau <aurora.griffith@students.mq.edu.au>
2023-12-04 13:10:44 +1100

    Update day2.scala
    done some code but getting 0, time for of buggin

--- src/main/scala/day2/day2.scala
+++ src/main/scala/day2/day2.scala
@@ -47,17 +47,96 @@ object Day2 {
 
         // println(inputLines.toString())
 
-        for(game <- inputLines){
+        // da patterns
+
+        // var gameLineMatch = "Game ([0-9]+): (([0-9]+) (red|green|blue).? ?)+".r
+        var numberMatcher = "[0-9]+".r
+        var gameLineNumberMatcher = "Game ([0-9]+):".r
+        // needs match all
+        var gameIterMatcher = "(([0-9]+) (red|green|blue)(, )?)+;?".r
+        var gameChoiceMatcher = "([0-9]+) (red|green|blue)".r
+        var gameChoiceColorMatcher = "(red|green|blue)".r
+
+        // for extraction of num
+        def getFirstNumberFromString(inputStringToRipFrom:String):Int ={
+          numberMatcher.findFirstIn(inputStringToRipFrom) match {
+            case None => -65536
+            case Some(thing) => {
+              try{
+                thing.toInt
+              }catch {
+                case e:Exception=> {
+                  println("had a spooker on ripped number toInt transofmrmering: "+thing)
+                  -65536 // spookable number
+                }
+              }
+            }
+          }
+        }
+        // the result val
+        var runningGameNumberTotal = 0
+        // loop all game liness
+        for(gameLine <- inputLines){
           // ...
-          println(game)
+          // for debugging
+          // println(gameLine)
           // ...
           // // ("Game \\d: ([a-zA-Z0-9]+)".r)(gameNumber,gameDetails)
           // game match {
           //   case "Game "+gameNumber+": "+gameDetails => printf("have the: %s\nwith: %s",gameNumber,gameDetails)
           // }
-          
-          // ...
+
+
+          try{
+            // grab the game label
+            val gameLineLabelString = gameLineNumberMatcher.findFirstIn(gameLine) match {
+              case None => "???"
+              case Some(thing) => thing
+            }
+            // grab the number from it
+            val gameNumber = getFirstNumberFromString(gameLineLabelString)
+            // grab the iterations
+            val gameIterationsList = gameIterMatcher.findAllIn(gameLine)
+            
+            var gameNumberIsSafe = true // assume fine, thisisfine.gif
+
+            for(gameIteration <- gameIterationsList){
+              // println(gameIteration)
+              // grab the selectables
+              val gameIterationSelections = gameChoiceMatcher.findAllIn(gameIteration)
+              for(selection <- gameIterationSelections){
+                // color handle
+                if(!(
+                  (gameChoiceColorMatcher.findFirstIn(selection) match { case None => "???" case Some(thing) => thing}) match {
+                    case "red" => getFirstNumberFromString(selection)>12
+                    case "green" => getFirstNumberFromString(selection)>13
+                    case "blue" => getFirstNumberFromString(selection)>14
+                    case spookyInput => {
+                      println("!!!! SPOOKY COLOUR !!!!: "+spookyInput)
+                      true // // do nothing else?? pretend fine?
+                    }
+                  }
+                )){
+                  // make the spooky checker
+                  gameNumberIsSafe = false
+                }
+              }
+            }
+
+            if(gameNumberIsSafe) {
+              runningGameNumberTotal=runningGameNumberTotal+gameNumber
+            }
+
+          } catch {
+            case e:Exception=> println("failed the handle lines: "+e.toString())
+          }
+
+          // done game line
         }
+
+        // done all games
+
+        printf("we got: %d\n",runningGameNumberTotal)
       }
       case numberInput => {
         Main.failingMessage("DAY 2 INVALID PART NUMBER: "+numberInput)
