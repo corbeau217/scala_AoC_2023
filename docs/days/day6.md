76df1b1fd110e3b97ba2524e2278c5792a5f090a
Corbeau <aurora.griffith@students.mq.edu.au>
2023-12-19 23:50:51 +1100

    day6pt1 done

--- docs/days/day6.md
+++ docs/days/day6.md
@@ -13,6 +13,10 @@ scala attempt at day 6
 ### strategy
 1. mathables
 2. probably just hard code but maybe by now we have a parser we can just run it through?
+3. boat racing thingy
+4. yoink from day5 to process the input data??
+5. nah just rip the numbers from it as a vector then deal
+6. oh wow ez star
 ## part 2
 ### strategy
 1. ...
--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -1,6 +1,7 @@
 package day6
 
 import scala.io.Source
+import org.bitbucket.inkytonik.kiama.output.PrettyPrinter.{any, layout}
 import Main._
 
 object Day6 {
@@ -52,6 +53,75 @@ object Day6 {
 
   def handlePart1(inputLines:List[String],includeDebuggingInfo:Boolean):Unit={
     // TODO: DAY 6 PART 1
+    var intMatcher = "[1-9][0-9]*".r
+    // prepare vectors of numbers
+    var raceTimes = Vector[Int]()
+    var raceDistances = Vector[Int]()
+    // strip the numbers
+    for(item <- intMatcher.findAllIn(inputLines(0))){
+      raceTimes=raceTimes:+item.toInt
+    }
+    for(item <- intMatcher.findAllIn(inputLines(1))){
+      raceDistances=raceDistances:+item.toInt
+    }
+
+    if(includeDebuggingInfo) {
+      print("race Times: ")
+      raceTimes.map(item=>printf("[%3d]",item))
+      println()
+    }
+    if(includeDebuggingInfo) {
+      print("race dists: ")
+      raceDistances.map(item=>printf("[%3d]",item))
+      println()
+    }
+
+    // both should be same length
+    val raceCount = raceTimes.length
+
+    // access all races finding the min and maximum hold time
+    // =============================================================================
+
+    var minHeldArray = new Array[Int](raceCount)
+    var maxHeldArray = new Array[Int](raceCount)
+    var rangeSizeArray = new Array[Int](raceCount)
+
+    // every race index
+    for(currRaceIndex <- 0 to raceCount-1){
+      // set the starting min and max to be worst case
+      minHeldArray(currRaceIndex) = Int.MaxValue
+      maxHeldArray(currRaceIndex) = Int.MinValue
+
+      // assume 1
+      var millisHeld = 1
+
+      // while we still at least 1 second for the movement
+      while( millisHeld < raceTimes(currRaceIndex)){
+        var raceTimeRemaining = raceTimes(currRaceIndex)-millisHeld
+
+        // when we can use the time remaining to cross the finish line at our current speed
+        if(raceTimeRemaining*millisHeld > raceDistances(currRaceIndex)){
+          // try for updates
+
+          // check if we havent found min yet
+          if(minHeldArray(currRaceIndex) > millisHeld) minHeldArray(currRaceIndex) = millisHeld
+          // otherwise if max needss updating
+          if(maxHeldArray(currRaceIndex) < millisHeld) maxHeldArray(currRaceIndex) = millisHeld
+        }
+
+        // prepare for next
+        millisHeld = millisHeld+1
+      }
+      // stash the combinationss
+      rangeSizeArray(currRaceIndex) = maxHeldArray(currRaceIndex) - minHeldArray(currRaceIndex) + 1
+
+      if(includeDebuggingInfo) printf("race[%d] -- min: %3d, max: %3d, num: %3d\n",currRaceIndex,minHeldArray(currRaceIndex),maxHeldArray(currRaceIndex), rangeSizeArray(currRaceIndex))
+    }
+    // get the product of all our race combinations and print
+    // =============================================================================
+    printf("RESULT SHOULD BE: %d\n",rangeSizeArray.product)
+    // =============================================================================
+    
   }
 
   // ========================================
